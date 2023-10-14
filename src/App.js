349649fb23de8837df347999079a9774c14bb380
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2023-10-15 04:08:31 +0800

    retrieve data from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import backendAPI from "./api/backendapi";
 import './App.css';
 
 function App() {
+  const [ menus, setMenus ] = useState([]);
+
+  useEffect(() => {
+    backendAPI.get('/menus')
+    .then((response) => {
+      console.log('GET status', response.status);
+      setMenus(response.data)
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Menu</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>   
+        </thead>   
+        <tbody>
+          {
+            menus.map((menu,key) =>
+            <tr key={key}>
+                <td className='table-data'>{menu.name }</td>
+                <td className='table-data'>{menu.price }</td>
+            </tr>)
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
